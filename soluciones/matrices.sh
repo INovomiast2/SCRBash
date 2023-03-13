72c3f27b7eae12bc9e567c94eb65c91bfba3dfea
INovomiast-IESFuengirola1 <116105316+INovomiast-IESFuengirola1@users.noreply.github.com>
2023-03-13 18:58:05 +0100

    Create matrices.sh

---
+++ soluciones/matrices.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+declare -a NUMEROS
+NUMEROS= ( cero uno dos tres )
+echo ${NUMEROS[2]}
+NUMEROS[4]="cuatro"
+echo ${NUMEROS[4]}
+NUMEROS= ( [6]=seis siete [9]=nueve )
+echo ${NUMEROS[7]}
+echo ${NUMEROS[*]}
+
+#Script by: INovomiat
