9a04d45afafa9d345e9dac007e1d6ec91e32576c
INovomiast-IESFuengirola1 <116105316+INovomiast-IESFuengirola1@users.noreply.github.com>
2023-03-13 18:43:54 +0100

    Create holamundo.sh

---
+++ soluciones/holamundo.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#
+# Hola Mundo con Bash
+
+MENSAJE="Hola Mundo!" #Variable donde se almacena la String.
+echo $MENSAJE #echo = mostrar en pantalla un valor | $MENSAJE = Variable a ser mostrada con dicho valor
+
+#Script by: INovomiast
