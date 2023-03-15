c25d9947752d9c52b0f12ef7c9b6b61be4826dbd
INovomiast-IESFuengirola1 <116105316+INovomiast-IESFuengirola1@users.noreply.github.com>
2023-03-15 18:37:14 +0100

    Update operadores_fechas.sh

--- soluciones/operadores_fechas.sh
+++ soluciones/operadores_fechas.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+[ -N "$1" ] && echo "Fichero $1 ha sido modificado con posterioridad a su ultima lectura"
+[ -N "$2" ] && echo "Fichero $2 ha sido modificado con posterioridad a su ultima lectura"
+[ "$1" -nt "$2" ] && echo "$1 es más reciente que $2"
+[ "$1" -ot "$2" ] && echo "$2 es más reciente que $1"
+
+#Script by: INovomiast
