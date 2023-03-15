e3a4002b5da2768e566ecde1447acd96316b95ea
INovomiast-IESFuengirola1 <116105316+INovomiast-IESFuengirola1@users.noreply.github.com>
2023-03-15 19:15:51 +0100

    Update infous.sh

--- soluciones/infous.sh
+++ soluciones/infous.sh
@@ -4,3 +4,7 @@ TEMPORAL=`grep "^$1:" /etc/passwd 2>/dev/null`
 USUARIO=`echo TEMPORAL | cut -f1 -d:`
 
 echo "Username: $USUARIO"
+echo -n "Identificador (UID): "
+echo $TEMPORAL | cut -f3 -d:  
+echo -n "Nombre del grupo primario: "
+GID=`echo $TEMPORAL | cut -f4 -d:`
