bd5873523186a6c540eb73216adf536263b6f0f7
INovomiast-IESFuengirola1 <116105316+INovomiast-IESFuengirola1@users.noreply.github.com>
2023-03-15 19:01:50 +0100

    Update combinando-expresiones.sh

--- soluciones/combinando-expresiones.sh
+++ soluciones/combinando-expresiones.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-palabra1 = ""
-palabra2 = ""
-palabra3 = ""
+palabra1 = "Hola"
+palabra2 = "Adios"
+palabra3 = "Hola"
 
-[ "" = "" -a "" = "" ] && echo ""
-[ "" = "" -a "" = "" ] && echo ""
-[ "" = "" -a "" = "" ] && echo ""
+[ "$palabra1" = "$palabra2" -a "$palabra2" = "$palabra3" ] && echo "Las 3 palabras coinciden."
+[ "$palabra1" = "$palabra2" -o "$palabra2" = "$palabra3" ] && echo "Algunas palabras coinciden."
+!([ "$palabra1" = "$palabra2" ]) && echo "Las palabras 1 y 2 son diferentes."
+
+#Script by:INovomiast
