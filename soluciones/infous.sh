e34cf30f013fd959476d4f1b23d72f94411cfe7d
INovomiast-IESFuengirola1 <116105316+INovomiast-IESFuengirola1@users.noreply.github.com>
2023-03-15 19:23:18 +0100

    Update infous.sh

--- soluciones/infous.sh
+++ soluciones/infous.sh
@@ -8,3 +8,6 @@ echo -n "Identificador (UID): "
 echo $TEMPORAL | cut -f3 -d:  
 echo -n "Nombre del grupo primario: "
 GID=`echo $TEMPORAL | cut -f4 -d:`
+grep ":$GID:" /etc/group | cut -f1 -d:
+echo "Directorio Personal: "
+ls -ld `echo $TEMPORAL | cut -f6 -d:`
