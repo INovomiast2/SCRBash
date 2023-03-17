c4a1cdaeeb167c231cf691ac41cc21bcd6a4b73d
INovomiast <inovdie1602@iesfuengirola1.es>
2023-03-17 09:00:17 +0100

    All Loading Modal parts are asembled, let's do the logic

---
+++ scripts/_js/loader.js
@@ -0,0 +1,36 @@
+/**
+ * ./scripts/_js/loader.js
+ */
+//Loader Script v.1.0
+//Dev: INovomiast-IESFuengirola1
+
+//Declare all variables with the elements on screen for the loader
+
+//Modal
+const lmodal = document.querySelector('#loading-modal');
+
+//Main Content
+const mcontent = document.querySelector('#main-content');
+
+//Loader li
+const lml_f = document.querySelector('#lm-load-f');
+const lml_s = document.querySelector('#lm-load-s');
+const lml_t = document.querySelector('#lm-load-t');
+const lml_fo = document.querySelector('lm-load-fo');
+const lml_fi = document.querySelector('lm-load-fi');
+
+//Loader Spinner
+const lml_f_sp = document.querySelector('#lm-load-f-spn');
+const lml_s_sp = document.querySelector('#lm-load-s-spn');
+const lml_t_sp = document.querySelector('#lm-load-t-spn');
+const lml_fo_sp = document.querySelector('#lm-load-fo-spn');
+const lml_fi_sp = document.querySelector('#lm-load-fi-spn');
+
+//Loader ChkMark
+const lml_f_chk = document.querySelector('#lm-load-f-chk');
+const lml_s_chk = document.querySelector('#lm-load-f-chk');
+const lml_t_chk = document.querySelector('#lm-load-f-chk');
+const lml_fo_chk = document.querySelector('#lm-load-f-chk');
+const lml_fi_chk = document.querySelector('#lm-load-f-chk');
+
+//On page load: Open Modal and start loading Proccess!
