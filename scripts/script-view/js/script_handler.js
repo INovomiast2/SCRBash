48736e11d1517900cb053c23de83c67ada4a8cb1
INovomiast-IESFuengirola1 <116105316+INovomiast-IESFuengirola1@users.noreply.github.com>
2023-03-27 18:21:05 +0000

    ScriptHandling - Casi completo

--- scripts/script-view/js/script_handler.js
+++ scripts/script-view/js/script_handler.js
@@ -31,6 +31,8 @@ const scrP3 = ["reverse.sh","arg_error.sh","arg_error_else.sh","greaterthan.sh",
 
 if (dir == dirs[0]) {
     for (x in scr3f) {
-        console.log(scr3f[x]);
+        if (script == scr3f[x]) {
+            getGitHubScript(`https://raw.githubusercontent.com/INovomiast-IESFuengirola1/SCRBash/main/Soluciones/Scripts_3/${script}`)
+        }
     }
 }
