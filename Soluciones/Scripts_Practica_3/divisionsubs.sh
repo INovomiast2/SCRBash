133e8db95fb6a7817f5ac16bce236007032f6b18
INovomiast-IESFuengirola1 <116105316+INovomiast-IESFuengirola1@users.noreply.github.com>
2023-03-30 03:46:38 +0200

    Coded: divsionsubs.sh - Completed! - Working

--- Soluciones/Scripts_Practica_3/divisionsubs.sh
+++ Soluciones/Scripts_Practica_3/divisionsubs.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+if [ $# -ne 2 ] ; then
+    exit 1
+fi
+
+#Variables
+dividendo=$1
+divisor=$2
+cociente=0
+
+#Logica
+while [ $dividendo -ge $divisor ] ; do
+    dividendo=$(($dividendo - $divisor))2>/dev/null
+    cociente=$(($cociente + 1))2>/dev/null
+done
+
+echo "El resultado de la división es: " $cociente
 
 
 #Script by: INovomiast
